d729e5a63c48fab52ea43e016660d40e1c648c63
Andruw-SO <andruw@studio-orange-world.com>
2024-09-12 09:19:08 +0200

    small modification

--- src/Screens/vendor/VendorDashboard.jsx
+++ src/Screens/vendor/VendorDashboard.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
-import { Text } from 'react-native-paper';
-import Cars from './Cars'
+import React from 'react';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Cars from './Cars';
 import BookingRequests from './BookingRequests';
-import Analytics from './Analytics'
+import Analytics from './Analytics';
 const Tab = createMaterialTopTabNavigator();
 const VendorDashboard = () => {
-   
-  return (
-    <Tab.Navigator>
-    <Tab.Screen  name="Cars" component={Cars} />
-    <Tab.Screen  name="Bookings" component={BookingRequests} />
-    <Tab.Screen  name="Analytics" component={Analytics} />
-    </Tab.Navigator>
-  )
-}
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="Cars" component={Cars} />
+			<Tab.Screen name="Bookings" component={BookingRequests} />
+			<Tab.Screen name="Analytics" component={Analytics} />
+		</Tab.Navigator>
+	);
+};
 
-export default VendorDashboard
+export default VendorDashboard;
